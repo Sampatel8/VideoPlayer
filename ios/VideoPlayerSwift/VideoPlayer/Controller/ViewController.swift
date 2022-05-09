883ca3477bf9d36edf4aee20a05ca117848684fd
Smit Patel <34744262+Sampatel8@users.noreply.github.com>
2022-05-08 21:57:45 -0400

    BasicUI Implementation Completed

---
+++ ios/VideoPlayerSwift/VideoPlayer/Controller/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Smit Patel on 2022-05-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var videoView: UIView!
+    
+    @IBOutlet weak var titileLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var detailsLabel: UILabel!
+    
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func perviousButtonTapped(_ sender: Any){
+    }
+    @IBAction func playButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+    }
+    
+}
+
